a0c6f0fe7965db3cbd5c70c3781a80653c8c6836
Suchanyecz Alex <suhalex04@gmail.com>
2023-03-14 19:04:46 +0100

    headerkesz

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector('#hamburger');
 const navDivs = document.querySelectorAll('.navDiv');
+const nav = document.querySelector('nav');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
+    nav.classList.toggle('active');
     navDivs.forEach(navDiv => navDiv.classList.toggle('active'));
 });
