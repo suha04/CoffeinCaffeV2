4675105d1a3e53de7e256743e601abb782ca6ca0
Suchanyecz Alex <suhalex04@gmail.com>
2023-03-14 21:16:31 +0100

    hiddennav

--- assets/js/function.js
+++ assets/js/function.js
@@ -7,3 +7,16 @@ hamburger.addEventListener('click', () => {
     nav.classList.toggle('active');
     navDivs.forEach(navDiv => navDiv.classList.toggle('active'));
 });
+
+
+  let prevScrollpos = window.pageYOffset;
+
+  window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.querySelector(".fixed-nav").classList.remove("hide-nav");
+    } else {
+      document.querySelector(".fixed-nav").classList.add("hide-nav");
+    }
+    prevScrollpos = currentScrollPos;
+  }
