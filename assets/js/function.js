3583a8b7138ad133ab306e50f441d86becfe0cdb
Suchanyecz Alex <suhalex04@gmail.com>
2023-03-15 00:30:23 +0100

    rolunkkeszul

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,22 +1,10 @@
 const hamburger = document.querySelector('#hamburger');
 const navDivs = document.querySelectorAll('.navDiv');
 const nav = document.querySelector('nav');
+const navList = document.getElementById("navList");
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     nav.classList.toggle('active');
     navDivs.forEach(navDiv => navDiv.classList.toggle('active'));
 });
-
-
-  let prevScrollpos = window.pageYOffset;
-
-  window.onscroll = function() {
-    let currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-      document.querySelector(".fixed-nav").classList.remove("hide-nav");
-    } else {
-      document.querySelector(".fixed-nav").classList.add("hide-nav");
-    }
-    prevScrollpos = currentScrollPos;
-  }
