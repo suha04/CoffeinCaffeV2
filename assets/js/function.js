48ee74b28c229608358ed969f76c542f7141ae86
Suchanyecz Alex <suhalex04@gmail.com>
2023-03-15 12:50:33 +0100

    navmodositas

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,10 +1,34 @@
 const hamburger = document.querySelector('#hamburger');
 const navDivs = document.querySelectorAll('.navDiv');
 const nav = document.querySelector('nav');
-const navList = document.getElementById("navList");
+
+const currentScrollPos = window.pageYOffset;
+
+
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     nav.classList.toggle('active');
     navDivs.forEach(navDiv => navDiv.classList.toggle('active'));
 });
+
+
+
+
+
+window.onscroll = function() {
+    let prevScrollpos = window.pageYOffset;
+
+  if (prevScrollpos == currentScrollPos) {
+    // Show the navigation bar
+    nav.classList.remove("hidden");
+  } 
+  else {
+    // Hide the navigation bar
+    hamburger.classList.remove('active');
+    navDivs.forEach(navDiv => navDiv.classList.remove('active'));
+    nav.classList.remove('active');
+  }
+  
+  prevScrollpos = currentScrollPos;
+}
