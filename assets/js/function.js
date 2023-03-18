120b769d248b2e3f9e816c8cee928de226be1f5a
Suchanyecz Alex <suhalex04@gmail.com>
2023-03-18 12:29:14 +0100

    szendvicsekkeszul2

--- assets/js/function.js
+++ assets/js/function.js
@@ -4,6 +4,7 @@ const hamburger = document.querySelector('#hamburger');
 const navDivs = document.querySelectorAll('.navDiv');
 const nav = document.querySelector('nav');
 
+
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     nav.classList.toggle('active');
@@ -33,7 +34,7 @@ window.onscroll = function() {
 
 
 
-//carousel
+//Carousel
 
 const carousel = document.querySelector('.carousel');
 const slides = carousel.querySelector('.slides');
@@ -81,3 +82,45 @@ carousel.querySelector('.prev').addEventListener('click', handlePrevClick);
 carousel.querySelector('.next').addEventListener('click', handleNextClick);
 
 updateSlideCounter();
+
+
+
+
+//Aktív nav elemek
+
+const navLinks = document.querySelectorAll('#navList a');
+
+// Linkek figyelése klikkhez
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    // aktív eltávolítása minden navelemről
+    navLinks.forEach(link => link.classList.remove('active'));
+
+    // klikkelt elemek aktívvá tétele
+    link.classList.add('active');
+  });
+});
+
+// első nav elemhez .active, oldaltöltéskor
+navLinks[0].classList.add('active');
+
+// aktív elem frissítés görgetésnél
+window.addEventListener('scroll', () => {
+  // görgetés pozíció
+  const scrollPos = window.scrollY;
+
+  // minden nav elem figyelembe vétele
+  navLinks.forEach(link => {
+    // link hivatkozása
+    const anchor = document.querySelector(link.hash);
+
+    // hivatkozás létezik és legalább 10vh-re van-e a felső szélétől
+    if (anchor && anchor.getBoundingClientRect().top < window.innerHeight - window.innerHeight * 0.5) {
+      // aktív eltávolítása minden navelemről
+      navLinks.forEach(link => link.classList.remove('active'));
+
+      // aktív hozzáadása jelenlegi navelemhez
+      link.classList.add('active');
+    }
+  });
+});
